8741d8fd9aa5cf078af4efae08fe394eae8125d4
Will Luong <tluong@stedwards.edu>
2020-11-03 16:37:55 -0600

    Update the file with Python3 implementation to generate unique permutation using Backtracking

--- permutation.py
+++ permutation.py
@@ -45,6 +45,66 @@ def permute(nums):
   return result
 
 
+#Problem 47: Permutations II
+'''
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+Example 1:
+
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+Example 2:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 8
+-10 <= nums[i] <= 10
+
+'''
+from collections import Counter
+#function to provide the soltuion to the problem
+def permuteUnique(nums):
+  #base case:
+  if not nums:  #no list, no return
+    return None
+  
+  result = []
+
+  #a helper method to dfs through the list of number and duplicates to generate the unique permutation
+  def backtracking(vistied, numsFreq):
+    #base case: 
+    if len(vistied) == len(numsFreq):
+      #make a deep copy of the permutation as we would backtrack and check on it 
+      result.append(list(vistied))
+      return
+
+    #loop through the nums list 
+    for num in numsFreq: 
+      #the number currently has element to be processed
+      if numsFreq[num] > 0:
+        #add it into the visited array and processed it 
+        vistied.append(num)
+        numsFreq[num] -= 1
+
+        #continue to explore 
+        backtracking(vistied, numsFreq)
+
+        #revert the choice for the next iteration
+        vistied.pop()
+        numsFreq[num] += 1
+
+  backtracking([], Counter(nums))
+  return result
+
+
+
+
 
 #main function to run the program 
 def main():
@@ -53,5 +113,11 @@ def main():
   test_nums_2 = [4,5,6,8]
   print(permute(test_nums_1))
   print(permute(test_nums_2))
+
+  print("TESTING PERMUTATION II...")
+  test_nums_3 = [1,1,2]
+  test_nums_4 = [1,2,3]
+  print(permuteUnique(test_nums_3))
+  print(permuteUnique(test_nums_4))
   print("END OF TESTING....")
 main()
